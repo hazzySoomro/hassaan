a4cc20089328066bcba3721036908b55f55c0b6c
Hassaan <98661403+hazzySoomro@users.noreply.github.com>
2024-01-20 14:45:35 +0500

    error fix

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
 	return (
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
 			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>Lets Make&nbsp;</h1><br/>
-				<h1 className={title({ color: "violet" })}> your beautiful&nbsp;</h1>
+				<h1 className={title()}>Lets Make</h1><br/>
+				<h1 className={title({ color: "violet" })}> your beautiful</h1>
 				<br />
 				<h1 className={title()}>
 					websites regardless of your design and development knowledge.
