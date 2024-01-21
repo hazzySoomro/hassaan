f10757cdd3bb134ed5641c5c0ce5746b6767042e
Hassaan <98661403+hazzySoomro@users.noreply.github.com>
2024-01-21 18:43:47 +0500

    project1

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,6 +1,9 @@
 import { title } from "@/components/primitives";
 import {Divider} from "@nextui-org/divider";
 import React from "react";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code"
+import {Chip} from "@nextui-org/react";
 
 export default function PricingPage() {
 	return (
@@ -8,11 +11,20 @@ export default function PricingPage() {
 				<div>
 					<h1 className={title()}>Projects and Reviews</h1>
 					<Divider className="my-4 w-full"/>
-					<p className="items-center flex justify-center">uploading soon ...</p>
+					<p className="items-center flex justify-center">gathering all 6years of projects and will be uploading soon ...</p>
+					<br/>
+					
 				</div>
 
-				<div>
-			{/* <h2>hello</h2> */}
+				<div className="mt-24 flex gap-6 justify-center items-center">
+				<Chip color="warning" variant="bordered">Stay connected</Chip>
+					<Snippet hideSymbol hideCopyButton variant="flat">
+					<span>
+						<a href="https://wa.me/+923312444229" target="_blank">
+						Let&apos;s talk <Code color="success">whatsapp</Code>
+						</a>
+					</span>
+				</Snippet>
 				
 				</div>
 		</div>
