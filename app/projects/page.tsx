99d21be2a792e24d42dc8de769b6f46e353d0a88
Hassaan <98661403+hazzySoomro@users.noreply.github.com>
2024-01-21 18:25:38 +0500

    a

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,13 +1,66 @@
 import { title } from "@/components/primitives";
 import {Divider} from "@nextui-org/divider";
+import React from "react";
+import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue} from "@nextui-org/react";
+
+
+const rows = [
+	{
+	  key: "1",
+	  name: "Tony Reichert",
+	  role: "CEO",
+	  status: "Active",
+	},
+	{
+	  key: "2",
+	  name: "Zoey Lang",
+	  role: "Technical Lead",
+	  status: "Paused",
+	},
+	{
+	  key: "3",
+	  name: "Jane Fisher",
+	  role: "Senior Developer",
+	  status: "Active",
+	},
+	{
+	  key: "4",
+	  name: "William Howard",
+	  role: "Community Manager",
+	  status: "Vacation",
+	},
+  ];
+  
+  const columns = [
+	{
+	  key: "name",
+	  label: "NAME",
+	},
+	{
+	  key: "role",
+	  label: "ROLE",
+	},
+	{
+	  key: "status",
+	  label: "STATUS",
+	},
+  ];
 
 export default function PricingPage() {
 	return (
 		<div>
-			<h1 className={title()}>Projects and Reviews</h1>
-			<Divider className="my-4 w-full"/>
-			<p className="items-center flex justify-center"> hello</p>
+				<div>
+					<h1 className={title()}>Projects and Reviews</h1>
+					<Divider className="my-4 w-full"/>
+					<p className="items-center flex justify-center"> hello</p>
+				</div>
+
+				<div>
+			<h2>hello</h2>
+				
+				</div>
 		</div>
+		
 		
 	);
 }
